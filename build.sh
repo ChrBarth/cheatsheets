c0b962f11e628ee8d0dbe244518c06bc396b4402
ChrBarth <ChrBarth@users.noreply.github.com>
2019-03-07 20:15:44 +0100

    use xelatex for pdf creation

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@
 # and .pdf files in pdf-directory
 for mdfile in *.md
 do
+echo "Converting $mdfile..."
 pandoc -o html/${mdfile%.md}.html $mdfile
-pandoc -o pdf/${mdfile%.md}.pdf $mdfile
+pandoc --latex-engine=xelatex -o pdf/${mdfile%.md}.pdf $mdfile
 # http://www.tldp.org/LDP/abs/html/string-manipulation.html
+# xelatex is needed so pandoc can compile the unicode-cheatsheet
+# (although most characters are missing anyway...)
 done
 
