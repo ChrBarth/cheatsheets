b34c4b7c3a15dfdda088acc76960011c5684f042
ChrBarth <ChrBarth@users.noreply.github.com>
2020-10-17 22:14:49 +0200

    pandoc update

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 for mdfile in *.md
 do
 echo "Converting $mdfile..."
-pandoc -H ~/Dokumente/vorlagen/default_css.html -o html/${mdfile%.md}.html $mdfile
-pandoc --latex-engine=xelatex -o pdf/${mdfile%.md}.pdf $mdfile
+pandoc -H ~/Dokumente/vorlagen/default_css.html --metadata pagetitle="${mdfile%.md}" -o html/${mdfile%.md}.html $mdfile
+pandoc --pdf-engine=pdflatex -o pdf/${mdfile%.md}.pdf $mdfile
 # http://www.tldp.org/LDP/abs/html/string-manipulation.html
 # xelatex is needed so pandoc can compile the unicode-cheatsheet
 # (although most characters are missing anyway...)
