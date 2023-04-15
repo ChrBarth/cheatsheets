5c8567800fee3ab8910892ef4d1540c1d57256b3
Christoph Barth <ChrBarth@users.noreply.github.com>
2023-04-15 14:08:17 +0200

    do not create unicode-pdf since latex can not handle that...

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ for mdfile in *.md
 do
 echo "Converting $mdfile..."
 pandoc -H ~/Dokumente/vorlagen/default_css.html --metadata pagetitle="${mdfile%.md}" -o html/${mdfile%.md}.html $mdfile
-pandoc --pdf-engine=pdflatex -o pdf/${mdfile%.md}.pdf $mdfile
+if [[ $mdfile == "unicode-cheatsheet.md" ]]
+then
+    # latex has problems with unicode-characters:
+    echo "skipping $mdfile..."
+else
+    pandoc --pdf-engine=pdflatex -o pdf/${mdfile%.md}.pdf $mdfile
+fi
 # http://www.tldp.org/LDP/abs/html/string-manipulation.html
 # xelatex is needed so pandoc can compile the unicode-cheatsheet
 # (although most characters are missing anyway...)
